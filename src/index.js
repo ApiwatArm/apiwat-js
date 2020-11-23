6b800d10000cd6365b77aac25c1b8c9385ea2236
ApiwatArm <71430128+ApiwatArm@users.noreply.github.com>
2020-11-23 11:26:27 +0700

    async await

--- src/index.js
+++ src/index.js
@@ -10,15 +10,16 @@ function simulateAsyncAPI(text, time) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log(text);
+
       resolve();
     }, time);
   });
 }
 
-simulateAsyncAPI("A", 1000)
-  .then(() => {
-    return simulateAsyncAPI("B", 500);
-  })
-  .then(() => {
-    return simulateAsyncAPI("C", 100);
-  });
+async function run() {
+  await simulateAsyncAPI("A", 1000);
+  await simulateAsyncAPI("B", 500);
+  await simulateAsyncAPI("C", 100);
+}
+
+run();
